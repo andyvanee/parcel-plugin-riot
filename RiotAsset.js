cda81b1afc2ded02788a0c6ee79a3c8f29e215fe
Andy Vanee <1andyvanee@gmail.com>
2018-06-08 09:12:33 -0600

    Fix Dependency on JSAsset
    References:
    
    https://github.com/parcel-bundler/parcel/issues/1319
    
    https://github.com/albinotonnina/parcel-plugin-inlinesvg/commit/fca116e4a101c50452376a791d7c7778a5e4573c#diff-b81103fdda0f9372603f751178ceb604

--- RiotAsset.js
+++ RiotAsset.js
@@ -1,17 +1,25 @@
 const { compile } = require('riot-compiler');
-const JSAsset = require('parcel-bundler/src/assets/JSAsset');
+const {Asset} = require('parcel-bundler');
 const preamble = "const riot = require('riot');\n";
 
-class RiotAsset extends JSAsset {
-
+class RiotAsset extends Asset {
+  constructor(name, pkg, options) {
+    super(name, pkg, options);
+    this.type = 'js';
+  }
+  
   async parse(inputCode) {
     const riotOpts = {};
-
     let code = compile(inputCode, riotOpts, this.name);
     code = `${ preamble }${ code }`;
     this.contents = code;
-
-    return super.parse(this.contents);
+  }
+  
+  generate() {
+    // Send to JS bundler
+    return {
+      'js': `module.exports = \`${this.contents}\``
+    };
   }
 }
 
